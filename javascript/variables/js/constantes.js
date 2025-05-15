ea88b27499413e66f6de6d2c3c81962f91e9e2ef
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-05-14 20:47:44 -0400

    importar clases por defecto y alias para las importaciones

--- javascript/variables/js/constantes.js
+++ javascript/variables/js/constantes.js
@@ -3,4 +3,12 @@ export const PI = Math.PI
 export let usuario = 'Juan'
 let password = 'qwerty'
 
+export function saludar() {
+  console.log('Hola Módulos +ES6')
+}
 
+export default class Saludar {
+  constructor() {
+    console.log('Homa Modulos +ES6 desde una clase')
+  }
+}
--- javascript/variables/js/modulos.js
+++ javascript/variables/js/modulos.js
@@ -1,7 +1,11 @@
-import { PI, usuario } from './constantes.js'
-import { aritmetica } from './aritmetica.js'
+import Saludar, { PI, saludar, usuario } from './constantes.js'
+import { aritmetica as aritm } from './aritmetica.js'
 
 console.log('Archivo modulos.js')
 console.log(PI)
 console.log(usuario)
-console.log(aritmetica.sumar(3, 4))
+console.log(aritm.sumar(3, 4))
+
+saludar()
+
+let saludo = new Saludar()
